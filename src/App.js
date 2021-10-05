6a8c04bfe691e3c36478c34c7703601d0360e875
Colin Cason <48043196+colinjcason@users.noreply.github.com>
2021-10-04 20:54:42 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
